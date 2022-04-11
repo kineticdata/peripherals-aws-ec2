bb05574b6719609c8eb7b37ad6c115ce6722e72f
Chad Rehm <chad.rehm@kineticdata.com>
2022-04-11 14:44:01 -0500

    PER-269 remove unneeded file Several files were removed or updated after the replacement of the Legacy adapter and the addition of the current adapter.

--- bridge-adapters/kinetic-bridgehub-adapter-amazonec2/src/main/java/com/kineticdata/bridgehub/adapter/amazonec2/AmazonEC2QualificationParser.java
+++
@@ -1,9 +0,0 @@
-package com.kineticdata.bridgehub.adapter.amazonec2;
-
-import com.kineticdata.bridgehub.adapter.QualificationParser;
-
-public class AmazonEC2QualificationParser extends QualificationParser {
-    public String encodeParameter(String name, String value) {
-        return value;
-    }
-}
